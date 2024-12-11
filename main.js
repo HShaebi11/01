e7dafb5da720ad3cb56851299d6e1bba47c3d32c
HShaebi11 <hshaebi.11@gmail.com>
2024-12-11 22:35:09 +0000

    red div

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 alert("Hello World");
+
+function addRedDiv() {
+    const newDiv = document.createElement('div');
+    newDiv.style.backgroundColor = 'red';
+    document.body.appendChild(newDiv);
+}
+
+addRedDiv();
