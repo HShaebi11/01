c7283f61daa6195a32dc03d318659230c4110c91
HShaebi11 <hshaebi.11@gmail.com>
2024-12-11 22:38:20 +0000

    no red div

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 alert("Hello World");
-
-function addRedDiv() {
-    const newDiv = document.createElement('div');
-    newDiv.style.backgroundColor = 'red';
-    document.body.appendChild(newDiv);
-}
-
-addRedDiv();
